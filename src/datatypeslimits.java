d810fdf0ab33b51f72b0eb6bb2baa8c91d3ddb7d
Aditya <aparora13@gmail.com>
2021-11-21 15:21:23 +0530

    initial commit

---
+++ src/datatypeslimits.java
@@ -0,0 +1,24 @@
+public class datatypeslimits {
+    public static void main(String args[])
+    {
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MIN_VALUE;
+        float e = Float.MAX_VALUE;
+        float f = Float.MIN_VALUE;
+        byte g = Byte.MAX_VALUE;
+        byte h = Byte.MIN_VALUE;
+        double i = Double.MAX_VALUE;
+        double j = Double.MIN_VALUE;
+        short k = Short.MAX_VALUE;
+        short l = Short.MIN_VALUE;
+        long m = Long.MAX_VALUE;
+        long n = Long.MIN_VALUE;
+
+        System.out.println("Integer -  "+a+" to "+b);
+        System.out.println("Float -   "+e+" to "+f);
+        System.out.println("Byte -   "+g+" to "+h);
+        System.out.println("Double -   "+i+" to "+j);
+        System.out.println("Short -   "+k+" to "+l);
+        System.out.println("Long -   "+m+" to "+n);
+    }
+}
